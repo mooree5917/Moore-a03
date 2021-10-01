1ab6944632ad14dd342fb6c39a52626e2a370a4d
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-01 14:56:15 -0400

    First Commit

---
+++ exercise24/src/main/java/baseline/solution24.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 24 Solution
+ *  Copyright 2021 Eric Moore
+ */
+/*
+Create a program that compares two strings and determines if the two strings are anagrams.
+The program should prompt for both input strings and display the output as shown in the example that follows.
+Implement the program using a function called isAnagram,
+which takes in two words as its arguments and returns true or false.
+ */
+
+package baseline;
+
+public class solution24 {
+    public static void main(String[] args){
+
+    }
+}
---
+++ exercise25/src/main/java/baseline/solution25.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 25 Solution
+ *  Copyright 2021 Eric Moore
+ */
+/*
+   Create a program that determines the complexity of a given password based on these rules:
+   A very weak password contains only numbers and is fewer than eight characters.
+   A weak password contains only letters and is fewer than eight characters.
+   A strong password contains letters and at least one number and is at least eight characters.
+   A very strong password contains letters, numbers, and special characters and is at least eight characters.
+   If a password does not meet any of these rules, then report it as a password of unknown strength.
+ */
+package baseline;
+
+public class solution25 {
+    public static void main(String[] args){
+
+    }
+}
