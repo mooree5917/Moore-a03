415323c21dfe8128c3f40d3ae6c7eaaaf919a26a
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-03 00:52:31 -0400

    added files

--- exercise28/src/main/java/baseline/solution28.java
+++ exercise28/src/main/java/baseline/solution28.java
@@ -1,2 +1,13 @@
-package baseline;public class solution28 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 28 Solution
+ *  Copyright 2021 Eric Moore
+ * Write a program that prompts the user for five numbers and computes the total of the numbers.
+ * This must use repetition.
+ */
+package baseline;
+
+public class solution28 {
+    public static void main(String[] args){
+
+    }
 }
--- exercise29/src/main/java/baseline/solution29.java
+++ exercise29/src/main/java/baseline/solution29.java
@@ -1,2 +1,18 @@
-package baseline;public class solution29 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 29 Solution
+ *  Copyright 2021 Eric Moore
+ *
+ * Write a quick calculator that prompts for the rate of return on an investment
+ * and calculates how many years it will take to double your investment.
+ * Formula -> years = 72 / r (r is the stated rate of return.)
+ * Don’t allow the user to enter 0.
+ * Don’t allow non-numeric values.
+ * Use a loop to trap bad input, so you can ensure that the user enters valid values.
+ */
+package baseline;
+
+public class solution29 {
+    public static void main(String[] args){
+
+    }
 }
