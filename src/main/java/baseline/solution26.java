2270b30ac1f39a08f170673d37a1e4394784db62
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-02 22:43:15 -0400

    added exercise 26

---
+++ src/main/java/baseline/solution26.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 26 Solution
+ *  Copyright 2021 Eric Moore
+ */
+/*
+    Write a program that will help you determine how many months it will take to pay off a credit card balance.
+    The program should ask the user to enter the balance of a credit card,
+    the APR of the card, and their monthly payment.The program should then return
+    the number of months needed (rounded up to the next integer value).
+    Formula: n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+ */
+package baseline;
+
+public class solution26 {
+    public static void main(String[] args){
+
+        /*PaymentCalculator class
+        n is the number of months.
+        i is the daily rate (APR divided by 365).
+        b is the balance.
+        p is the monthly payment.
+        */
+
+        //Public method calculateMonthsUntilPaidOff
+    }
+}
