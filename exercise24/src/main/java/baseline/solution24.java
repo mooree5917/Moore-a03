277e73da1b9a6cd58ea2004632796eee670ec113
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-03 23:47:49 -0400

    added code to exercise 24

--- exercise24/src/main/java/baseline/solution24.java
+++ exercise24/src/main/java/baseline/solution24.java
@@ -1,4 +1,4 @@
-/*
+package baseline;/*
  *  UCF COP3330 Fall 2021 Assignment 24 Solution
  *  Copyright 2021 Eric Moore
  */
@@ -8,11 +8,51 @@ The program should prompt for both input strings and display the output as shown
 Implement the program using a function called isAnagram,
 which takes in two words as its arguments and returns true or false.
  */
+import java.util.Arrays;
+import java.util.Scanner;
 
 package baseline;
 
 public class solution24 {
-    public static void main(String[] args){
+    static boolean isAnagram(String str1, String str2)
+    {
+        // Get lengths of both strings
+        int n1 = str1.length;
+        int n2 = str2.length;
 
+        // If length of both strings is not same,
+        // then they cannot be anagram
+        if (n1 != n2)
+            return false;
+
+        // Sort both strings
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        // Compare sorted strings
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
+
+        return true;
     }
+
+    public static void main(String[] args)
+    {
+
+        //Get the first anagram.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first String");
+        String str1 = sc.next();
+
+        System.out.println("Enter your second String");
+        String str2 = sc.next();
+
+        if (isAnagram(str1, str2))
+            System.out.println(str1 + "and" + str2 + "are anagrams");
+        else
+            System.out.println(str1 + "and " + str2 + "are not anagrams");
+    }
+
+
 }
